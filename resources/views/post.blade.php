a9644694ca8b0ad672483863aa90b1f6fd8c54cd
Melanie Burger <melanie.brgr@gmail.com>
2021-07-11 18:19:49 +0200

    🎉 Route wildcards

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,7 +2,8 @@
 <title>My blog</title>
 <link rel="stylesheet" href="/app.css">
 <body>
-  <h1><a href="/post">first post</a></h2>
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in lacus elit. Duis pulvinar non nibh ut eleifend. Cras vitae lorem non enim ultrices bibendum. Donec eu lorem ac risus commodo fermentum. Pellentesque pretium tempus dui quis consectetur. Curabitur purus eros, auctor ultricies pellentesque at, aliquam sit amet sem. Sed sed sem eu dui lacinia laoreet in eu ex. Pellentesque varius tortor eget quam molestie, in rutrum ipsum convallis. Nam faucibus tellus in est volutpat bibendum. Maecenas bibendum cursus velit, nec dictum enim porta ac. Vestibulum tortor enim, mattis sit amet magna ut, viverra tincidunt elit. Nam lobortis posuere ipsum a varius.</p>
+  <article>
+    <?= $post; ?>
+  </article>
   <a href="/">go back</a>
 </body>
