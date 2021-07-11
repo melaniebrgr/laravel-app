439ce714e4e1370d5f32cf3d4e96af2c3309d52c
Melanie Burger <melanie.brgr@gmail.com>
2021-07-11 19:36:27 +0200

    🎉 Caches

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,10 @@ Route::get('/', function () {
 Route::get('/posts/{post}', function ($slug) {
     $path = __DIR__ . "/../resources/posts/$slug.html";
     if (! file_exists($path)) {
-        abort(404);
+        return redirect('/');
     }
-    $post = file_get_contents($path);
+
+    $post = cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
 
     return view('post', [
         'post' => $post
