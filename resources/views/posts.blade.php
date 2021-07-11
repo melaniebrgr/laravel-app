6d2f9e222fee4ef2a9f20060cc97252a3c0d9ea3
Melanie Burger <melanie.brgr@gmail.com>
2021-07-11 17:56:16 +0200

    🎉 Inits post page

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<title>My blog</title>
+<link rel="stylesheet" href="/app.css">
+<body>
+  <h1>My blog</h1>
+  <h2><a href="/post">first post</a></h2>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in lacus elit. Duis pulvinar non nibh ut eleifend. Cras vitae lorem non enim ultrices bibendum. Donec eu lorem ac risus commodo fermentum. Pellentesque pretium tempus dui quis consectetur. Curabitur purus eros, auctor ultricies pellentesque at, aliquam sit amet sem. Sed sed sem eu dui lacinia laoreet in eu ex. Pellentesque varius tortor eget quam molestie, in rutrum ipsum convallis. Nam faucibus tellus in est volutpat bibendum. Maecenas bibendum cursus velit, nec dictum enim porta ac. Vestibulum tortor enim, mattis sit amet magna ut, viverra tincidunt elit. Nam lobortis posuere ipsum a varius.</p>
+  <h2><a href="/post">second post</a></h2>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in lacus elit. Duis pulvinar non nibh ut eleifend. Cras vitae lorem non enim ultrices bibendum. Donec eu lorem ac risus commodo fermentum. Pellentesque pretium tempus dui quis consectetur. Curabitur purus eros, auctor ultricies pellentesque at, aliquam sit amet sem. Sed sed sem eu dui lacinia laoreet in eu ex. Pellentesque varius tortor eget quam molestie, in rutrum ipsum convallis. Nam faucibus tellus in est volutpat bibendum. Maecenas bibendum cursus velit, nec dictum enim porta ac. Vestibulum tortor enim, mattis sit amet magna ut, viverra tincidunt elit. Nam lobortis posuere ipsum a varius.</p>
+  <h2><a href="/post">third post</a></h2>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in lacus elit. Duis pulvinar non nibh ut eleifend. Cras vitae lorem non enim ultrices bibendum. Donec eu lorem ac risus commodo fermentum. Pellentesque pretium tempus dui quis consectetur. Curabitur purus eros, auctor ultricies pellentesque at, aliquam sit amet sem. Sed sed sem eu dui lacinia laoreet in eu ex. Pellentesque varius tortor eget quam molestie, in rutrum ipsum convallis. Nam faucibus tellus in est volutpat bibendum. Maecenas bibendum cursus velit, nec dictum enim porta ac. Vestibulum tortor enim, mattis sit amet magna ut, viverra tincidunt elit. Nam lobortis posuere ipsum a varius.</p>
+</body>
