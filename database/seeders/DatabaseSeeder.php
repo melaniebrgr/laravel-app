5f169cffa0d20ebaa7fed11b6b7bdac2a2f84260
Melanie Burger <melanie.brgr@gmail.com>
2021-07-18 20:32:49 +0200

    ♻️ Uses factories for seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,45 +16,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-
-        Category::truncate();
-
-        Post::truncate();
-
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
+        $user = User::factory()->create([
+            'name' => 'Jane Doe'
         ]);
 
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-
-        $hobbies = Category::create([
-            'name' => 'Hobbies',
-            'slug' => 'hobbies'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $personal->id,
-            'title' => 'My Personal Post',
-            'slug' => 'my-personal-post',
-            'excerpt' => 'Lorem ipsum dolor sit ament, conosecteur adipiscing elit.',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in lacus elit. Duis pulvinar non nibh ut eleifend. Cras vitae lorem non enim ultrices bibendum. Donec eu lorem ac risus commodo fermentum. Pellentesque pretium tempus dui quis consectetur. Curabitur purus eros, auctor ultricies pellentesque at, aliquam sit amet sem.'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $hobbies->id,
-            'title' => 'My Hobbies Post',
-            'slug' => 'my-hobbies-post',
-            'excerpt' => 'Lorem ipsum dolor sit ament, conosecteur adipiscing elit.',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in lacus elit. Duis pulvinar non nibh ut eleifend. Cras vitae lorem non enim ultrices bibendum. Donec eu lorem ac risus commodo fermentum. Pellentesque pretium tempus dui quis consectetur. Curabitur purus eros, auctor ultricies pellentesque at, aliquam sit amet sem.'
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
     }
 }
