833dde013135351242c88d79ca4fbf89d4c3a24b
Melanie Burger <melanie.brgr@gmail.com>
2021-07-18 20:33:22 +0200

    ♻️ Passes foreign key for author

--- resources/views/post.php
+++ resources/views/post.php
@@ -8,7 +8,7 @@
       <?= $post->title; ?>
     </h1>
       <p>
-          By <a href="#">Melanie Burger</a> in <a href="/categories/<?= $post->category->slug ?>"><?= $post->category->name ?></a>
+          By <a href="#"><?= $post->author->name ?></a> in <a href="/categories/<?= $post->category->slug ?>"><?= $post->category->name ?></a>
       </p>
     <p><?= $post->body; ?></p>
   </article>
