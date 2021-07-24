ec6dda759756375166c43544a89711bece754d18
Melanie Burger <melanie.brgr@gmail.com>
2021-07-24 13:09:43 +0200

    ♻️ Uses blade templating

---
+++ resources/views/post.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<title>My blog</title>
+<link rel="stylesheet" href="/app.css">
+<body>
+  <h1>My blog</h1>
+  <article>
+    <h1>
+      {{ $post->title }}
+    </h1>
+      <p>
+          By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+      </p>
+    <p>{{ $post->body }}</p>
+  </article>
+  <a href="/">go back</a>
+</body>
